9761a9bb4dc18de986e8c43c0c7445a0dd86df1e
Gregor Dschung <dschung@cs.uni-kl.de>
2015-02-08 16:47:16 +0100

    [Sync] New project for gathering the relevant data from the existing webservice

---
+++ Mensa-KL.Sync/ISyncClient.cs
@@ -0,0 +1,14 @@
+﻿using Mensa_KL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensa_KL.Sync
+{
+    public interface ISyncClient
+    {
+        Task<List<Meal>> FetchMeals(DateTime start, DateTime end);
+    }
+}
---
+++ Mensa-KL.Sync/MensaKLSyncClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensa_KL.Sync
+{
+    public class MensaKLSyncClient : ISyncClient
+    {
+        public Task<List<Models.Meal>> FetchMeals(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
