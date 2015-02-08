f6b486220c82d1fa5e51b141bb213eb79c80c6d3
Gregor Dschung <dschung@cs.uni-kl.de>
2015-02-08 16:50:30 +0100

    [BL] MensaClient is used to compound Mensa-KL.Sync and Mensa-KL.DAL

---
+++ Mensa-KL.BL/MensaClient.cs
@@ -0,0 +1,34 @@
+﻿using Mensa_KL.DAL;
+using Mensa_KL.Sync;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mensa_KL.BL
+{
+    public class MensaClient
+    {
+        private readonly ISyncClient _syncClient;
+        private readonly MealDataManager _mealDataManager; 
+
+        public MensaClient() : this(null) { }
+
+        internal MensaClient(ISyncClient syncClient)
+        {
+            if (syncClient != null)
+            {
+                this._syncClient = syncClient;
+            }
+            else
+            {
+                this._syncClient = new MensaKLSyncClient();
+            }
+        }
+
+        public void UpdateMeals(DateTime start, DateTime end)
+        {
+            _syncClient.FetchMeals(start, end);
+        }
+    }
+}
---
+++ Mensa-KL.Tests/BLTests.cs
@@ -0,0 +1,37 @@
+﻿using Mensa_KL.BL;
+using Mensa_KL.Models;
+using Mensa_KL.Sync;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensa_KL.Tests
+{
+    [TestFixture]
+    public class BLTests
+    {
+        [Test]
+        public void TestUpdateMeals()
+        {
+            var syncClient = Substitute.For<ISyncClient>();
+
+            var dateStart = new DateTime(2015,1,1);
+            var dateEnd = new DateTime(2015,1,2);
+
+            var meal1 = new Meal { MealId = 1, Date = new DateTime(2015, 1, 1), Location = Location.MENSA_1, Title = "Schmorbraten" };
+            var meal2 = new Meal { MealId = 2, Date = new DateTime(2015, 1, 2), Location = Location.MENSA_2, Title = "Blunzegrötschl" };
+            var meals = new List<Meal> { meal1, meal2 };
+
+            syncClient.FetchMeals(Arg.Any<DateTime>(), Arg.Any<DateTime>()).Returns(Task.FromResult(meals));
+
+            var mensaClient = new MensaClient(syncClient);
+            mensaClient.UpdateMeals(dateStart, dateEnd);
+
+            syncClient.Received().FetchMeals(Arg.Any<DateTime>(), Arg.Any<DateTime>());
+        }
+    }
+}
